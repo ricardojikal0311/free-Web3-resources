45067f58aaf0c10d6c5367f830c528dc4100eb87
ricardojikal0311 <15992876+ricardojikal0311@users.noreply.github.com>
2022-04-19 18:10:31 +0000

    fix: environment setting

--- restore.sh
+++ restore.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Set start and end date
-START_DATE="2023-01-01"  # YYYY-MM-DD
-END_DATE="2023-11-18"    # YYYY-MM-DD
+START_DATE="2022-04-01"  # YYYY-MM-DD
+END_DATE="2022-08-18"    # YYYY-MM-DD
 
 # Min and max commits per selected day
 MIN_COMMITS=1
